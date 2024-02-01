974feba2cd0eaf15b2b589bc11ef85c09c400368
윤예리 <yoonr721@gmail.com>
2024-02-01 23:09:15 +0900

    1476 윤예리

--- 1회차/Beakjoon_1476/baekjoon_1476_윤예리.py
+++ 1회차/Beakjoon_1476/baekjoon_1476_윤예리.py
@@ -1,4 +1,10 @@
 # E는 15까지, S는 28까지, M은 19까지
 E, S, M = map(int, input().split())
 
-
+i = max([E, S, M])
+while i > 0:
+    if (i - E) % 15 == 0 and (i - S) % 28 == 0 and (i - M) % 19 == 0:
+        print(i)
+        break
+    else:
+        i += 1
