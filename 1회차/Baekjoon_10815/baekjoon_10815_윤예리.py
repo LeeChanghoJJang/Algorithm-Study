b8ed4c9539b82e2bb6d33d1796241fb9623ca5bf
윤예리 <yoonr721@gmail.com>
2024-02-02 22:04:02 +0900

    10815 1929 수정 윤예리

--- 1회차/Baekjoon_10815/baekjoon_10815_윤예리.py
+++ 1회차/Baekjoon_10815/baekjoon_10815_윤예리.py
@@ -1,10 +1,13 @@
 N = int(input())
-hv_card = list((map(int, input().split())))
+
+# 시간 초과 이슈로 가지고 있는 카드는 set로 설정하였음
+hv_card = set((map(int, input().split())))
 
 M = int(input())
+
+# table의 경우 순서가 바뀌면 result가 바뀌기 때문에 list로 설정
 table = list((map(int, input().split())))
 
-# 시간 초과
 result = []
 for card in table:
     if card in hv_card:
@@ -13,3 +16,4 @@ for card in table:
         result.append(0)
 
 print(*result)
+
--- 1회차/Baekjoon_1929/baekjoon_1929_윤예리.py
+++ 1회차/Baekjoon_1929/baekjoon_1929_윤예리.py
@@ -27,3 +27,22 @@ N, M = map(int, input().split())
 # for a in arr:
 #     if a != 0:
 #         print(a)
+
+# 에라토스테네스의 체
+def is_prime(num):
+    if num < 2:
+        return False
+
+    # i로 나누었을 때 나누어 떨어지면 i의 배수로도 나누어떨어짐
+    # 따라서 i**0.5 까지만 나누어봄
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(N, M+1):
+    if is_prime(i):
+        print(i)
+
+# 시간초과가 뜬 코드들의 시간복잡도: O(N^2)
+# 정답 코드의 시간복잡도: O(N * sqrt(M))
