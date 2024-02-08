68a13a135befe5633a8526e9d633a292faf4b54b
leechangho <ckdgh6589@naver.com>
2024-02-08 23:35:32 +0900

    240208 11053백준완료

--- 2회차/baekjoon_11053/baekjoon_11053_이창호.py
+++ 2회차/baekjoon_11053/baekjoon_11053_이창호.py
@@ -1,18 +1,20 @@
 # 백준 11053 가장 긴 증가하는 부분수열
-# 약간만 이해한 참이라 다시 한번 보고 진행 필요
+import sys
+sys.stdin = open('input.txt')
+
 N = int(input())
-sequence = map(int,input().split())
+sequence = list(map(int,input().split()))
 dp = [1]*N
-for i in range(N):
+for i in range(1,N):
     for j in range(i):
-        if sequence[i] > sequence[j]:
+        if sequence[j] < sequence[i]:
             dp[i] = max(dp[i],dp[j]+1)
-print(max(dp))
+print(dp)
 
 
 '''
-10 1
-10 20 2
+10 1 DP[1] = 1
+10 20 2 DP[2] = max(DP[1], DP[2]) if DP[i] > DP[i-1]: DP[i] +=1
 10 20 10 2
 10 20 10 30 3
 10 20 10 30 20 3
