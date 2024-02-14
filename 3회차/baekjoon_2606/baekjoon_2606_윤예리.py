ab3f7f05abebcf0bf3e97e7770ae6f603c1a4742
윤예리 <yoonr721@gmail.com>
2024-02-15 00:33:09 +0900

    2606

---
+++ 3회차/baekjoon_2606/baekjoon_2606_윤예리.py
@@ -0,0 +1,19 @@
+computer = int(input())
+network = int(input())
+connection = [[] for _ in range(computer+1)]
+visited = [0] * (computer+1)
+
+# 그래프 만들기
+for n in range(network):
+    a, b = map(int, input().split())
+    connection[a].append(b)
+    connection[b].append(a)
+
+def dfs(s):
+    visited[s] = 1
+    for i in connection[s]:
+        if visited[i] == 0:
+            dfs(i)
+
+dfs(1)
+print(sum(visited)-1)
