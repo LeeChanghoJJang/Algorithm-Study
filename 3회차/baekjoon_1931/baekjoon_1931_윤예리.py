d1524c2c4d279f4508ba6ba4dc13852f2a87f0ac
윤예리 <yoonr721@gmail.com>
2024-02-16 00:10:31 +0900

    1931

---
+++ 3회차/baekjoon_1931/baekjoon_1931_윤예리.py
@@ -0,0 +1,18 @@
+# 빨리 끝나고,
+# 빨리 시작하는 사람들 순으로
+n = int(input())
+meeting = []
+for _ in range(n):
+    start_time, end_time = map(int, input().split())
+    meeting.append([start_time, end_time])
+# 끝나는 순으로 sort, 끝나는 시간이 같다면 시작 시간으로 sort
+meeting.sort(key= lambda x:(x[1], x[0]))
+
+count = 1
+end_time = meeting[0][1]
+
+for i in range(1, n):
+    if meeting[i][0] >= end_time:
+        count += 1
+        end_time = meeting[i][1]
+print(count)
