b304704d700dc36631806521a7929e36fd369e61
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-02-16 10:02:43 +0900

    Update baekjoon_2583_이창호.py
    주석달기

--- 3회차/baekjoon_2583/baekjoon_2583_이창호.py
+++ 3회차/baekjoon_2583/baekjoon_2583_이창호.py
@@ -1,18 +1,21 @@
 # baekjoon_2583 영역 구하기
 import sys
 sys.stdin = open('input.txt')
-from pprint import pprint
 from collections import deque
+# 델타 설정
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
+# 일단 주어진 조건대로 영역을 나눠보자
 def paint(x1,y1,x2,y2,graph):
     for i in range(x1, x2):
         for j in range(y1, y2):
             graph[i][j] =1
     return graph
-
+    
+# BFS를 활용해서 한번 0을 만난 경우, 인접한 것들도 전부 색칠해줌
 def BFS(x,y):
+    # area로 그 구역의 넓이 계산
     area = 1
     queue = deque([[x,y]])
     while queue:
@@ -27,16 +30,19 @@ def BFS(x,y):
                 area += 1
     return area
 
-# BFS 활용문제 인듯
+# M : 행 N : 열 K : 직사각형의 범위 
 M,N,K = map(int,input().split())
 graph = [[0] * N for _ in range(M)]
 
 for _ in range(K):
+    # 직사각형의 범위를 시작점 행,열과 도착점 행,열로 구분해서 언팩 
     y1,x1,y2,x2 = map(int,input().split())
+    # 직사각형으로 일단 전 구역 색칠
     graph = paint(x1,y1,x2,y2,graph)
 temp = []
 for i in range(M):
     for j in range(N):
+        # BFS를 활용해서 한번 0을 만난 경우, 인접한 것들도 전부 색칠해줌 ==> temp의 길이가 총 구역의 갯수이며, 각 원소는 그 구역의 넓이
         if graph[i][j] ==0:
             temp.append(BFS(i,j))
 print(len(temp))
