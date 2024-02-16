453a53811207d965106c055cb64233bc0e72c8bd
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-02-16 09:56:22 +0900

    Update baekjoon_1931_이창호.py
    주석달기

--- 3회차/baekjoon_1931/baekjoon_1931_이창호.py
+++ 3회차/baekjoon_1931/baekjoon_1931_이창호.py
@@ -3,24 +3,30 @@ import sys
 sys.stdin = open('input.txt')
 
 N = int(input())
+# 배정된 회의실을 시작점 기준으로 먼저 정렬하고, 도착점 기준으로 정렬한다.
+# 그리디 알고리즘을 적용할 것
 meetings = sorted([list(map(int,input().split())) for _ in range(N)],key=lambda x:(x[0],x[1]))
+# 가능한 회의시간을 담을 temp 설정
 temp = []
+# 미팅에서 시작점과 끝점을 모두 순회
 for start, end in meetings:
-    # 아무것도 없을 때
+    # 아무것도 없을 때는 일단 추가
     if not temp:
         temp.append([start,end])
-    # temp가 있고, temp의 마지막 원소의 end값보다 현재의 end값이 더 작은 경우
-    # 무조건 기존 꺼 빼내야댐
+    # temp가 있을 때는?
     elif temp:
+        # 시작시간은 오름차순이므로 뒤로 갈수록 시작시간은 같거나 더 늦음 가정
+        # 이번 회의 시간보다 이전 회의시간 끝날 때가 더 느리다?
+        # 그러면 이전꺼를 빼고 이번 꺼를 넣어서 최대한 회의시간 확보 필요
         if temp[-1][1] > end:
             temp.pop()
             temp.append([start,end])
-        # start,end가 같을 때 적어도 뒤 숫자보다는 커야함
-        elif temp[-1][1] <= end and start==end:
-            temp.append([start,end])
-        # 현재의 start보다는 작아야 삽입가능
+        # 이전 회의 시간이 현재의 start보다는 작아야 삽입가능
         elif temp[-1][1] <= start:
             temp.append([start, end])
-print(meetings)
-print(temp)
+'''
+59444KB / 288ms
+'''
+print(len(temp))
+
 
