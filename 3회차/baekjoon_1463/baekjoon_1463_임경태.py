847b13a3f893b0960acbdc53f0fe9bd34b84df04
lim kyoung tae <limkt0925@gmail.com>
2024-02-16 22:46:24 +0900

    240216 UPDATE

--- 3회차/baekjoon_1463/baekjoon_1463_임경태.py
+++ 3회차/baekjoon_1463/baekjoon_1463_임경태.py
@@ -11,11 +11,11 @@ N = int(input())
 DP = [0] * (N+1)
 
 for n in range(2, N+1):
-    if n % 3 == 0 and n % 2 == 0:
+    if not n % 3 and not n % 2:
         DP[n] = min(DP[n//3], DP[n//2]) + 1
-    elif n % 3 == 0:
+    elif not n % 3:
         DP[n] = min(DP[n-1], DP[n//3]) + 1
-    elif n % 2 == 0:
+    elif not n % 2:
         DP[n] = min(DP[n-1], DP[n//2]) + 1
     else:
         DP[n] = DP[n-1] + 1
@@ -30,9 +30,9 @@ DP = [0] * (N+1)
 
 for n in range(2, N+1):
     DP[n] = DP[n-1] + 1
-    if n % 2 == 0:
+    if not n % 2:
         DP[n] = min(DP[n], DP[n//2] + 1)
-    if n % 3 == 0:
+    if not n % 3:
         DP[n] = min(DP[n], DP[n//3] + 1)
 
 print(DP[N])
