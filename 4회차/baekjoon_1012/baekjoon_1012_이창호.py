63d818969a6c520599a0e9e66166d31994568f6b
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-02-23 10:12:25 +0900

    Update baekjoon_1012_이창호.py

--- 4회차/baekjoon_1012/baekjoon_1012_이창호.py
+++ 4회차/baekjoon_1012/baekjoon_1012_이창호.py
@@ -1,4 +1,4 @@
-# 1012번 유기농 배추
+# 1012번 유기농 배추 (34088KB / 72ms)
 from collections import deque
 T =int(input())
 # 인접하는 네방향의 단위벡터 정의 
@@ -9,17 +9,18 @@ dy = [1,-1,0,0]
 def bfs(a,b):
     que = deque()
     que.append([a,b])
+    # 방문 한곳은 다시 방문하지 않기 위해 배추가 없는 것처럼 0처리 
     graph[a][b]=0
-    
+    # 주변에 배추가 없을 때까지 탐색
     while que:
         x,y = que.popleft()
-        
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            
+            # 범위를 벗어나면 다음 차례로 이동 
             if nx < 0 or nx>= M or ny <0 or ny>=N:
                 continue
+            # 배추가 있으면 그 방향으로 탐색하기 위해 que에 좌표 추가
             if graph[nx][ny]:
                 que.append([nx,ny])
                 graph[nx][ny] =0
@@ -35,12 +36,8 @@ for _ in range(T):
     total_cnt = 0
     for i in range(M):
         for j in range(N):
-            # 배추가 있는 곳을 기준으로, 너비 우선 탐색 실시
+            # 배추가 있는 곳을 발견하면 BFS로 방문하고, 카운팅 
             if graph[i][j] == 1:
                 bfs(i,j)
                 total_cnt+=1
     print(total_cnt)
-'''코드리뷰
-bfs 코드 구현을 위해 사이트를 참조하여 풀었음.
-완벽히 bfs 코드 구현 후, 재도전 필요
-'''
