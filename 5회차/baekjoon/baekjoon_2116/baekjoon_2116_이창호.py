a2d9d530157cd8b96b1f2e48493a54a4c0f08548
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-02 22:21:49 +0900

    Update baekjoon_2116_이창호.py

--- 5회차/baekjoon/baekjoon_2116/baekjoon_2116_이창호.py
+++ 5회차/baekjoon/baekjoon_2116/baekjoon_2116_이창호.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin = open('input.txt')
 sys.setrecursionlimit(10**6)
+# col 색깔 하나가 정해지면 나머지 하나를 정해주는 함수
 def trans(col):
     if col in [1,3]:
         result = 4-col
@@ -9,7 +10,7 @@ def trans(col):
     elif col in [2,4]:
         result = 6-col
     return result
-
+# 주사위 한 면이 정해지면 일자로 세웠을 때 나머지 한면은 정해짐
 def dice(row,value,result):
     global max_val
     if row == T:
@@ -21,10 +22,14 @@ def dice(row,value,result):
         return
 
     for next_col in range(6):
+        # 이전 주사위의 반대편 면에 해당되는 값의 열 인덱스를 찾음
         if dicer[row][next_col] == value:
             col = next_col
+            # 거기에 반대위치에 있는 열
             col2 = trans(col)
+            # 그 열의 값 
             value2 = dicer[row][col2]
+            # 해당 열의값과 반대편의 값을 result에 추가
             result.append(eyes - {dicer[row][col],dicer[row][col2]})
             dice(row + 1, value2, result)
             result.pop()
