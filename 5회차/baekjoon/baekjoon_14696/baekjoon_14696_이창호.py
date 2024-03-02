79c8a29d47f3a94f810b7658c35216259d8c8071
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-02 22:13:24 +0900

    Update baekjoon_14696_이창호.py

--- 5회차/baekjoon/baekjoon_14696/baekjoon_14696_이창호.py
+++ 5회차/baekjoon/baekjoon_14696/baekjoon_14696_이창호.py
@@ -2,16 +2,21 @@
 # a는 1~100 b는 1~100
 import sys
 sys.stdin = open('input.txt')
+# A와 B를 매칭시켰을 때 승부여부를 가르는 함수
 def game(A,B):
+    # A와 B의 원소가 있을 때까지만
     while A and B:
+        # 각각 하나씩 받음
         pattern1 = A.pop()
         pattern2 = B.pop()
+        # 숫자가 크면 이기는 것
         if pattern1 > pattern2:
             return 'A'
         elif pattern1 < pattern2:
             return 'B'
 
     else:
+        # 만약 위에서 결판이 안나고 길이가 다르다면
         if len(A) > len(B):
             return 'A'
         elif len(A) < len(B):
