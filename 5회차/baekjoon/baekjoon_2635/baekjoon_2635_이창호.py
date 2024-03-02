3bbc8e88137bad5fae31222387584a632aa0c5dd
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-03 08:49:12 +0900

    Update baekjoon_2635_이창호.py

--- 5회차/baekjoon/baekjoon_2635/baekjoon_2635_이창호.py
+++ 5회차/baekjoon/baekjoon_2635/baekjoon_2635_이창호.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin =open('input.txt')
 N = int(input())
-
+# search 함수는 두 수가 주어질 때, 이어가는 수열을 반환하는 함수
 def search(num,N):
     temp = [N,num]
     while N:
@@ -13,6 +13,7 @@ def search(num,N):
 
 max_val = 0
 result = []
+# N보다 작은 수에 대해 모두 search 함수 돌리기 
 for i in range(N+1):
     if max_val < len(search(i,N)):
         max_val = len(search(i,N))
