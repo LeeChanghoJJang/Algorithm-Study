3df7788e0ba61126e84f5c480fd1998451ed8f52
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-03 08:48:08 +0900

    Update baekjoon_2628_이창호.py

--- 5회차/baekjoon/baekjoon_2628/baekjoon_2628_이창호.py
+++ 5회차/baekjoon/baekjoon_2628/baekjoon_2628_이창호.py
@@ -6,13 +6,17 @@ heights = [0,M]
 T = int(input())
 for tc in range(T):
     cut1, cut2  = map(int,input().split())
+    # 가로로 자르는 경우
     if cut1==0:
         width.append(cut2)
+    # 세로로 자르는 경우
     elif cut1 ==1:
         heights.append(cut2)
+# 순차적으로 넓이를 구하기 위해 소트
 width.sort()
 heights.sort()
 max_val = 0
+# 가로와 세로를 순차적으로 탐색 하여 길이 비교
 for i in range(1,len(width)):
     for j in range(1,len(heights)):
         if max_val < (width[i] - width[i-1]) * (heights[j] - heights[j-1]):
