34988e4b07b09a9ebcbd27e3a2e42808307b7e7f
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-10 11:48:55 +0900

    Update baekjoon_14235_이창호.py

--- 6회차/baekjoon_14235/baekjoon_14235_이창호.py
+++ 6회차/baekjoon_14235/baekjoon_14235_이창호.py
@@ -6,11 +6,16 @@ T= int(input())
 heap = []
 for tc in range(T):
     N, *gifts = map(int,input().split())
+    # N이 0이면 크리스마스 선물을 줘야함
     if N == 0:
+        # 줄게 있다면? 순차적으로 꺼내서 줌(단 가치가 큰것부터(최대힙))
         if heap:
             print(heapq.heappop(heap)[1])
+        # 없으면 뭐 -1이지
         else:
             print(-1)
+    # N이 0이 아니면 기프트에 추가되는 거임
     else:
+        # 한개가 아닐 수 있으므로 반복문을 통해 최대힙으로 저장 
         for i in gifts:
             heapq.heappush(heap,(-i,i))
