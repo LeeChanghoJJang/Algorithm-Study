03f8872bae7eb8091ba0808e6b699af7ee72f7ea
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-10 11:46:00 +0900

    Update baekjoon_15663_이창호.py

--- 6회차/baekjoon_15663/baekjoon_15663_이창호.py
+++ 6회차/baekjoon_15663/baekjoon_15663_이창호.py
@@ -1,8 +1,10 @@
 import sys
 sys.stdin = open('input.txt')
-
+# 순열을 위한 백트래킹 정의
 def backtracking(start, result):
     if len(result) == M:
+        # copy를 하지 않으면 result가 바뀔 때마다 계속 갱신됨
+        # 그 때의 result를 저장하고 싶어서임 
         temp.add(tuple(result.copy()))
         return
 
@@ -17,7 +19,9 @@ def backtracking(start, result):
 N, M = map(int, input().split())
 arr = sorted(map(int, input().split()))
 visited = [0] * (N + 1)
+# temp에 순열을 전부 저장
 temp = set()
 backtracking(0, [])
+# 순서대로 뽑기
 for i in sorted(temp):
     print(*i)
