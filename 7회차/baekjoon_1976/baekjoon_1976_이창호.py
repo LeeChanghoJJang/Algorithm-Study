64451c1bf9264fb1c8882cce6a2a2e81942b8340
이창호 <152049355+LeeChanghoJJang@users.noreply.github.com>
2024-03-15 22:26:22 +0900

    Update baekjoon_1976_이창호.py

--- 7회차/baekjoon_1976/baekjoon_1976_이창호.py
+++ 7회차/baekjoon_1976/baekjoon_1976_이창호.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin = open('input.txt')
 from collections import deque
+# BFS 구현(출발점에서 도착점 갈 수 있다면 True 반환)
 def BFS(start,end):
     visited = [0] * (N+1)
     queue = deque([start])
@@ -15,15 +16,18 @@ def BFS(start,end):
     return False
 
 N,M = int(input()), int(input())
+# 인접한 국가들
 travel = [[] for _ in range(N+1)]
 for i in range(N):
     temp = list(map(int, input().split()))
     for j in range(N):
         if temp[j]:
             travel[i].append(j)
+# 여행 경로 계획
 plan = list(map(lambda x : int(x)-1,input().split()))
 result = 'YES'
 for i in range(len(plan)-1):
+    # 한번이라도 다음경로로 가지 못한다면
     if not BFS(plan[i],plan[i+1]):
         result='NO'
         break
