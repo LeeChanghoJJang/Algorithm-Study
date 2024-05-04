73af572713531dd12f756a3448d063bcfe62b0ea
dltkdgus482 <dltkdgus482@naver.com>
2024-05-05 00:04:14 +0900

    2024-05-05 UPDATE

---
+++ 14회차/programmers_64061_크레인인형뽑기게임/programmers_ 64061_이상현.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+
+class Solution {
+	static ArrayDeque<Integer> stack = new ArrayDeque<>();
+	
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        int height = board[0].length;
+        
+        for (int i = 0; i < moves.length; i++) {
+        	int j = 0;
+        	while (j < height && board[j][moves[i] - 1] == 0) j++;
+        	if (j == height) continue;
+        	
+        	if (!stack.isEmpty() && stack.peek() == board[j][moves[i] - 1]) {
+        		stack.pop();
+        		answer += 2;
+        	} else {
+        		stack.push(board[j][moves[i] - 1]);
+        	}
+        	board[j][moves[i] - 1] = 0;
+        }
+        
+        return answer;
+    }
+}
