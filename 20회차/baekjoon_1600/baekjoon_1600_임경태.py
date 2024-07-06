efbf4b3c07a32106cbd0ad1fd607a46c6fa67809
임경태 <limkt0925@gmail.com>
2024-07-06 18:31:36 +0900

    240706 UPDATE

--- 20회차/baekjoon_1600/baekjoon_1600_임경태.py
+++ 20회차/baekjoon_1600/baekjoon_1600_임경태.py
@@ -7,35 +7,31 @@ grid = [input().split() for _ in range(H)]
 dist = [[[0] * W for _ in range(H)] for _ in range(K+1)]
 dist[0][0][0] = 1
 
-dp = ((1, 0), (0, 1), (-1, 0), (0, -1))
-dk = ((1, 2), (2, 1), (-2, 1), (-1, 2), (1, -2), (2, -1), (-1, -2), (-2, -1))
+dp = (1, 0), (0, 1), (-1, 0), (0, -1)
+dk = (1, 2), (2, 1), (-2, 1), (-1, 2), (1, -2), (2, -1), (-1, -2), (-2, -1)
 Q = deque([[0, 0, 0]])
 
-# BFS
+def move(cnt, i, j, dr, is_knight):
+    for di, dj in dr:
+        ni, nj = i + di, j + dj
+
+        if 0<=ni<H and 0<=nj<W and grid[ni][nj] != '1' and not dist[cnt+is_knight][ni][nj]:
+            dist[cnt+is_knight][ni][nj] = dist[cnt][i][j] + 1
+            Q.append([cnt+is_knight, ni, nj])
+
 while Q:
     cnt, i, j = Q.popleft()
     
     # 도착점에 도달했을 경우
-    if i == H-1 and j == W-1:
-        exit(print(dist[cnt][i][j] - 1))
+    if i == H-1 and j == W-1: exit(print(dist[cnt][i][j] - 1))
 
     # 기본 이동
-    for di, dj in dp:
-        ni, nj = i + di, j + dj
-
-        if 0<=ni<H and 0<=nj<W and grid[ni][nj] != '1' and not dist[cnt][ni][nj]:
-            dist[cnt][ni][nj] = dist[cnt][i][j] + 1
-            Q.append([cnt, ni, nj])
+    move(cnt, i, j, dp, 0)
 
     # 나이트 이동 가능 여부 확인
     if cnt == K: continue
 
     # 나이트 이동
-    for di, dj in dk:
-        ni, nj = i + di, j + dj
-
-        if 0<=ni<H and 0<=nj<W and grid[ni][nj] != '1' and not dist[cnt+1][ni][nj]:
-            dist[cnt+1][ni][nj] = dist[cnt][i][j] + 1
-            Q.append([cnt+1, ni, nj])
+    move(cnt, i, j, dk, 1)
 
 print(-1)
--- 20회차/baekjoon_2457/baekjoon_2457_임경태.py
+++ 20회차/baekjoon_2457/baekjoon_2457_임경태.py
@@ -1,30 +1,24 @@
 # 2457 공주님의 정원
 N = int(input())
-flowers = sorted(list(map(int, input().split())) for _ in range(N))
+flowers = sorted(tuple(map(int, input().split())) for _ in range(N))
 now_end = (3, 1)
-ans = 0
+max_end = (3, 1)
+ans = idx = 0
 
-for i in range(N):
-    # 각 꽃의 시작 월/일 및 끝 월/일
-    sm, sd, em, ed = flowers[i]
+while now_end <= (11, 30):
+    # 커버 가능 꽃 중 가장 멀리 끝나는 꽃을 찾기 위한 루프
+    while idx < N and (flowers[idx][0], flowers[idx][1]) <= now_end:
+        max_end = max(max_end, (flowers[idx][2], flowers[idx][3]))
+        idx += 1
 
-    # 현재 커버 중인 기간에 포함되는 경우
-    if (sm, sd) <= now_end < (em, ed):
-        max_end = (em, ed)
+    # 더 이상 커버할 수 있는 꽃이 없을 경우
+    if now_end == max_end: exit(print(0))
 
-        # 이후 꽃들을 순회하며 가장 긴 기간 찾기
-        for nsm, nsd, nem, ned in flowers[i+1:N]:
-            if now_end < (nsm, nsd): break
-            max_end = max(max_end, (nem, ned))
+    # 종료 시점 갱신
+    now_end = max_end
+    ans += 1
 
-        # 갱신
-        now_end = max_end
-        ans += 1
-
-        # 11월 30일 이후까지 커버할 수 있다면 결과 출력 후 종료
-        if now_end > (11, 30): exit(print(ans))
-
-print(0) # 모든 꽃을 커버하지 못한 경우
+print(ans)
 
 '''
     핵심 : 주어진 데이터를 효율적으로 관리하고, 꽃이 개화하는 구간을 정확하게 파악하는 것
